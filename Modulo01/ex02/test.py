973fd5b11ffd0c1d904e237bf81bec55e39eafbf
johnconh <sevenballs@gmail.com>
2023-02-24 16:37:04 +0100

    ok

--- Modulo01/ex02/test.py
+++ Modulo01/ex02/test.py
@@ -6,7 +6,7 @@
 #    By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/24 13:44:09 by jdasilva          #+#    #+#              #
-#    Updated: 2023/02/24 13:53:31 by jdasilva         ###   ########.fr        #
+#    Updated: 2023/02/24 16:35:22 by jdasilva         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,3 +16,35 @@ from vector import Vector
 v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
 v2 = v1 * 5
 print(v2)
+
+# Expected output:
+# Vector([[0.0], [5.0], [10.0], [15.0]])
+# Row vector of shape 1 * n
+v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+v2 = v1 * 5
+print(v2)
+
+# Expected output
+# Vector([[0.0, 5.0, 10.0, 15.0]])
+v2 = v1 / 2.0
+print(v2)
+# Expected output
+# Vector([[0.0], [0.5], [1.0], [1.5]])
+
+# Column vector of shape (n, 1)
+print(Vector([[0.0], [1.0], [2.0], [3.0]]).shape)
+# Expected output
+# (4,1)
+
+print(Vector([[0.0], [1.0], [2.0], [3.0]]).values)
+# Expected output
+# [[0.0], [1.0], [2.0], [3.0]]
+
+# Row vector of shape (1, n)
+print(Vector([[0.0, 1.0, 2.0, 3.0]]).shape)
+# Expected output
+# (1,4)
+
+print(Vector([[0.0, 1.0, 2.0, 3.0]]).values)
+# Expected output
+# [[0.0, 1.0, 2.0, 3.0]]
--- Modulo01/ex02/vector.py
+++ Modulo01/ex02/vector.py
@@ -6,7 +6,7 @@
 #    By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/24 12:28:07 by jdasilva          #+#    #+#              #
-#    Updated: 2023/02/24 13:39:19 by jdasilva         ###   ########.fr        #
+#    Updated: 2023/02/24 16:29:49 by jdasilva         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,10 +14,17 @@ class Vector:
     def __init__(self, values):
         self.values = values
         self.shape = self.get_shape()
+    def __str__(self):
+        return str(self.values)
     def get_shape(self):
         filas = len(self.values)
         columnas = len(self.values[0])
-        return(filas, columnas) if columnas > 1 else (1, filas)
+        if columnas == 1:
+            (1, filas)
+        else: 
+            (columnas, 1)
+        return(filas, columnas) 
+        
     def dot(self, other):
         if self.shape != other.shape:
             raise ValueError ("Shaper vector no son iguales")
@@ -26,25 +33,25 @@ class Vector:
             result += self.values[0][i] * other.values[0][1]
         return result
     def T(self):
-        values_T = [[self.values[i][j] for i in range(self.shape[0])] for j in range(self.shape[1])]
+        values_T = [[self.values[j][i] for j in range(self.shape[0])] for i in range(self.shape[1])]
         return Vector(values_T)
     def __add__(self, other):
         if self.shape != other.shape:
             raise ValueError("Shaper vector no son iguales")
-        values = [[self.values[i][j] + other.values[i][j] for i in range(self.shape[1])] for j in range(self.shape[0])]
+        values = [[self.values[i][j] + other.values[i][j] for j in range(self.shape[1])] for i in range(self.shape[0])]
         return Vector(values)
     def __sub__(self, other):
         if self.shape != other.shape:
             raise ValueError("Shaper vector no son iguales")
-        values = [[self.values[i][j] - other.values[i][j] for i in range(self.shape[1])] for j in range(self.shape[0])]
+        values = [[self.values[i][j] - other.values[i][j] for j in range(self.shape[1])] for i in range(self.shape[0])]
         return Vector(values)
     def __mul__(self, scalar):
-        values = [[self.values[i][j] * scalar for i in range(self.shape[1])] for j in range(self.shape[0])]
+        values = [[self.values[i][j] * scalar for j in range(self.shape[1])] for i in range(self.shape[0])]
         return Vector(values)
     def __rmul__(self, scalar):
         return self.__mul__(scalar)
     def __truediv__(self, scalar):
-        values = [[self.values[i][j] * scalar for i in range(self.shape[1])] for j in range(self.shape[0])]
+        values = [[self.values[i][j] / scalar for j in range(self.shape[1])] for i in range(self.shape[0])]
         return Vector(values)
     def __rtruediv__(self, scalar):
         raise ArithmeticError("Vectors cannot be divided by scalars from the right.")
